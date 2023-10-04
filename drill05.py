793f4fbb28b7c22302e4865c3df20d9c6306cf37
taejunji <qq00aa@naver.com>
2023-10-04 18:20:03 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t 값이 1을 넘으면 다시, 새로운 화살표가 랜덤 위치에 만들어짐

--- drill05.py
+++ drill05.py
@@ -27,7 +27,7 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx,hy
+    global hx, hy
     global sx, sy
     global t
     global action
@@ -36,22 +36,29 @@ def reset_world():
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
     action = 3
-    sx,sy = cx,cy
-    hx, hy = random.randint(0, TUK_WIDTH-50), random.randint(0, TUK_HEIGHT) # p2 끝점
+
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy
+    hx, hy = random.randint(0, TUK_WIDTH - 50), random.randint(0, TUK_HEIGHT)  # p2 끝점
     # hx, hy = 50, 50
     t = 0.0
+
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx,hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
 
 def update_world():
     global frame
-    global cx,cy
+    global cx, cy
     global t
     global action
     frame = (frame + 1) % 8
@@ -64,6 +71,10 @@ def update_world():
         cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x를 1-t:t의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy
         t += 0.001
+    else:
+        cx,cy = hx,hy
+        set_new_target_arrow()
+
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
